affc04cc259c9b1dc77e4fa5580934b73b358474
iitggithub <iitg182@gmail.com>
2023-02-28 06:41:53 +0000

    Added timeout for python socket connection

--- client.py
+++ client.py
@@ -2,14 +2,16 @@ import socket
 import sys
 
 server_address = "ineedmyip.com"
-port = 2222
+port = 22
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-sock.connect((server_address, port))
-
 try:
+    sock.settimeout(10)
+    sock.connect((server_address, port))
     client_address = sock.recv(1024)
     sys.stdout.write(client_address.decode())
+except socket.timeout:
+    sys.stdout.write("Timeout connecting to server.\n")
 finally:
     sock.close()
