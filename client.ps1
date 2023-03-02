88a3bde9c70aff867889fbd2c438c6778ff8e59d
iitggithub <iitg182@gmail.com>
2023-03-02 22:52:16 +0000

    Added documentation references to script

--- client.ps1
+++ client.ps1
@@ -3,10 +3,14 @@ $port = 22
 
 # If this is the first time running this script you may
 # need to execute the following command in an administrator
-# powershell prompt to allow unsigned powershell scripts to
-# execute.
+# powershell prompt to allow powershell scripts to
+# be executed.
 #
 # Set-ExecutionPolicy unrestricted
+#
+# For more information on Powershell execution policies,
+# see the following link:
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.3
 
 try {
 	$sock = New-Object System.Net.Sockets.TcpClient
