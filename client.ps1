e12e1e76ee5e9bb7fd5df3c447fbc632e03dcf1a
iitggithub <iitg182@gmail.com>
2023-03-02 22:46:35 +0000

    Updated powershell script so it outputs the correct IP address

--- client.ps1
+++ client.ps1
@@ -1,14 +1,21 @@
 $server_address = "ineedmyip.com"
 $port = 22
 
+# If this is the first time running this script you may
+# need to execute the following command in an administrator
+# powershell prompt to allow unsigned powershell scripts to
+# execute.
+#
+# Set-ExecutionPolicy unrestricted
+
 try {
 	$sock = New-Object System.Net.Sockets.TcpClient
-	$sock.ConnectTimeout(10000) # 10 second connection timeout
 	$sock.Connect($server_address, $port)
 	$stream = $sock.GetStream()
 	$buffer = New-Object System.Byte[] 1024
 	$read = $stream.Read($buffer, 0, $buffer.Length)
-	write-Output [System.Text.Encoding]::ASCII.GetString($buffer, 0, $read)
+	$data = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $read)
+	write-Output $data
 }
 finally {
 	$sock.Close()
